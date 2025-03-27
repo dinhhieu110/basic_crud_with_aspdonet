a47ae37d31de1f957850d59f6213404d5287aa9f
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-27 11:56:08 +0700

    [GS-2] Apis - Get all games & specific game

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,7 +1,17 @@
+using GameStore.Api.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDTO> games = [
+     new (1, "League Of Legends", "ESport", 69.99M, new DateOnly(2025,3,27)),
+     new (2, "CF", "Fighting", 50.99M, new DateOnly(2025,3,27)),
+     new (3, "Zing Speed", "Racing", 40.99M, new DateOnly(2025,3,27)),
+];
+
+app.MapGet("/games", () => games);
+app.MapGet("/games/{id}", (int id) => games.Find(game=>game.Id == id));
+
 
 app.Run();
