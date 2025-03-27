f15122a88e6c571d2d80b3d24cdafd3393ab36e5
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-27 13:11:10 +0700

    [GS-3] Api - Create new game

---
+++ GameStore.Api/DTOs/CreateGameDTO.cs
@@ -0,0 +1,9 @@
+namespace GameStore.Api.DTOs;
+
+public record class CreateGameDTO
+(
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+const string GetGameEndPointName = "GetSpecificGame";
+
 List<GameDTO> games = [
      new (1, "League Of Legends", "ESport", 69.99M, new DateOnly(2025,3,27)),
      new (2, "CF", "Fighting", 50.99M, new DateOnly(2025,3,27)),
@@ -11,7 +13,21 @@ List<GameDTO> games = [
 ];
 
 app.MapGet("/games", () => games);
-app.MapGet("/games/{id}", (int id) => games.Find(game=>game.Id == id));
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndPointName);
+app.MapPost("/games", (CreateGameDTO newGame) =>
+{
+    GameDTO game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+        );
+    games.Add(game);
+    // Do not get it yet
+    return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
+});
+
 
 
 app.Run();
