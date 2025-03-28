8999ffdf550ff7b263f8d2400ce72d5c42d76379
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-28 13:56:57 +0700

    [GS-12] Game categories endpoint

---
+++ GameStore.Api/DTOs/GenreDTO.cs
@@ -0,0 +1,4 @@
+namespace GameStore.Api.DTOs;
+
+public record class GenreDTO
+(int Id, string Name);
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -10,6 +10,8 @@ var app = builder.Build();
 
 
 app.MapGameEndpoints();
+app.MapGenreEndpoints();
+
 await app.MigrateDbAsync();
 
 app.Run();
