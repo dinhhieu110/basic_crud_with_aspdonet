daabf9be12b50e4f0c211fad772c9c62b19eac15
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-28 11:03:24 +0700

    [GS-8] Generate DB with Sqlite

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,12 +1,15 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
 
 app.MapGameEndpoints();
-
-
+app.MigrateDb();
 
 app.Run();
